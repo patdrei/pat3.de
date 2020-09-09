db591710e1be1a8cf92be67d55b70fd818ba2908
Patrick Dreier <patrick3dreier@googlemail.com>
2020-09-09 14:13:59 +0200

    gifs and layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 Project.destroy_all
 
-Project.create(name: "Cool Cucumbers", description: "A Wonderful Success with an Amazing Team", url: "http://www.coolcucumbers.de", html_id: "cucumber", internal: false, cta: "Be a Cool Cucumber", image_address: "coolcucumber.svg", gif_address: "CoCu.gif")
+Project.create(name: "Cool Cucumbers", description: "A Wonderful Success with an Amazing Team", url: "http://www.coolcucumbers.de", html_id: "cucumber", internal: false, cta: "Chop, shop, never stop", image_address: "coolcucumber.svg", gif_address: "CoCu.gif")
 
-Project.create(name: "Crazy Cake Club", description: "A Wonderful Success with an Amazing Team", url: "http://www.crazycake.club", html_id: "cake", internal: false, cta: "Brighten up a Day", image_address: "birthday-cake-solid.svg", gif_address: "CoCu.gif")
+Project.create(name: "Crazy Cake Club", description: "A Wonderful Success with an Amazing Team", url: "http://www.crazycake.club", html_id: "cake", internal: false, cta: "Brighten up a Day", image_address: "birthday-cake-solid.svg", gif_address: "crazycake.gif")
+
+Project.create(name: "Urban Coffee Club", description: "Amesome coffee everywhere, at any time.", url: "https://www.urbancoffeeclub.de/", html_id: "ucc", internal: false, cta: "Have a Cup", image_address: "ucc.svg", gif_address: "CoCu.gif")
